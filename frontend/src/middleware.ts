96b1644c113bc086b4a408e7eb33badda57f9d72
KGR33N <kgr33n.dev@gmail.com>
2025-07-23 20:57:13 +0200

    Leave only backend for seperate repositories

--- frontend/src/middleware.ts
+++
@@ -1,6 +0,0 @@
-import { defineMiddleware } from 'astro:middleware';
-
-export const onRequest = defineMiddleware((context, next) => {
-  // For static sites, skip server-side language detection
-  return next();
-});
